667de686dd4b0f15141cd02a1f57600c5a0aed5e
yanun <yanunyang@psp-power.com.tw>
2023-05-26 19:01:53 +0800

    rm gradient blur

--- Sources/Ditto/Extension/View.swift
+++ Sources/Ditto/Extension/View.swift
@@ -2,18 +2,16 @@ import SwiftUI
 
 @available(iOS 15, *)
 extension View {
-    public func foregroundLinearGradient(_ colors: [Color], start: UnitPoint = .topLeading, end: UnitPoint = .trailing, blur: CGFloat = 0) -> some View {
+    public func foregroundLinearGradient(_ colors: [Color], start: UnitPoint = .topLeading, end: UnitPoint = .trailing) -> some View {
         return self.overlay {
             LinearGradient(colors: colors, startPoint: start, endPoint: end)
-                .blur(radius: blur)
                 .mask { self }
         }
     }
     
-    public func backgroundLinearGradient(_ colors: [Color], start: UnitPoint = .topLeading, end: UnitPoint = .trailing, blur: CGFloat = 0) -> some View {
+    public func backgroundLinearGradient(_ colors: [Color], start: UnitPoint = .topLeading, end: UnitPoint = .trailing) -> some View {
         return self.background(
             LinearGradient(colors: colors, startPoint: start, endPoint: end)
-                .blur(radius: blur)
         )
     }
 }

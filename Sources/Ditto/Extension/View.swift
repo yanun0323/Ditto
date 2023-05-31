2a296abd8d6c8f88b01cfba9112d003e3be1c82e
yanun <yanunyang@psp-power.com.tw>
2023-05-31 19:06:26 +0800

    update gradient if no colors return self

--- Sources/Ditto/Extension/View.swift
+++ Sources/Ditto/Extension/View.swift
@@ -2,16 +2,26 @@ import SwiftUI
 
 @available(iOS 15, *)
 extension View {
+    @ViewBuilder
     public func foregroundLinearGradient(_ colors: [Color], start: UnitPoint = .topLeading, end: UnitPoint = .trailing) -> some View {
-        return self.overlay {
-            LinearGradient(colors: colors, startPoint: start, endPoint: end)
-                .mask { self }
+        if colors.count == 0 {
+            self
+        } else {
+            self.overlay {
+                LinearGradient(colors: colors, startPoint: start, endPoint: end)
+                    .mask { self }
+            }
         }
     }
     
+    @ViewBuilder
     public func backgroundLinearGradient(_ colors: [Color], start: UnitPoint = .topLeading, end: UnitPoint = .trailing) -> some View {
-        return self.background(
-            LinearGradient(colors: colors, startPoint: start, endPoint: end)
-        )
+        if colors.count == 0 {
+            self
+        } else {
+            self.background(
+                LinearGradient(colors: colors, startPoint: start, endPoint: end)
+            )
+        }
     }
 }

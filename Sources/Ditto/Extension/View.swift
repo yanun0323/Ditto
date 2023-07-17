ad71b3fa439373d408f64381816d46c6616eb419
yanun <yanunyang@psp-power.com.tw>
2023-07-18 00:25:06 +0800

    add view debug

--- Sources/Ditto/Extension/View.swift
+++ Sources/Ditto/Extension/View.swift
@@ -3,6 +3,11 @@ import SwiftUI
 @available(iOS 16, macOS 13, watchOS 9, *)
 extension View {
     @ViewBuilder
+    public func debug() -> some View {
+        self.border(Color.red, width: 1)
+    }
+    
+    @ViewBuilder
     public func frame(size: CGSize, alignment a: Alignment = .center ) -> some View {
         self.frame(width: size.width, height: size.height, alignment: a)
     }

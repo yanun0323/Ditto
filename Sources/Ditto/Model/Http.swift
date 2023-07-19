72dd399d36a017b81fd95dbdc89a44303de82cb0
yanun <yanunyang@psp-power.com.tw>
2023-07-19 16:12:32 +0800

    udpate Http methods

--- Sources/Ditto/Model/Http.swift
+++ Sources/Ditto/Model/Http.swift
@@ -1,9 +1,7 @@
 import Foundation
 
-@available(iOS 16, macOS 13, watchOS 9, *)
 public struct Http {}
 
-@available(iOS 16, macOS 13, watchOS 9, *)
 extension Http {
     public enum Method: String {
         case GET = "GET"
@@ -14,51 +12,97 @@ extension Http {
     }
 }
 
-@available(iOS 16, macOS 13, watchOS 9, *)
+extension Http {
+    public enum Error {
+        case errParseURL
+        case errDownloadData(String)
+        case errDecodeData(String)
+        
+        var message: String {
+            switch self {
+                case .errParseURL:
+                    return "parse URL error, please check your url path"
+                case let .errDownloadData(msg):
+                    return "download data error, err: \(msg)"
+                case let .errDecodeData(msg):
+                    return "decode json data error, err: \(msg)"
+            }
+        }
+    }
+}
+
+extension Http.Error: Error {}
+
 extension Http {
     /**
      Send a request and get structure response
      
      ```swift
      let url = "http://api/user"
-     let user = Http.SendRequest(.GET, toUrl: url, type: User.self) { request in
-     // do something ...
+     let (user, statusCode, err) = Http.SendRequest(.GET, toUrl: url, type: User.self, debug: false) { request in
+        // do something ...
      }
      ```
+     - parameters:
+        - method: The http method for this request. (GET, POST, PUT, PATCH, DELETE)
+        - toUrl: The url to send request.
+        - action: Use this to modify request, such as set cookies, add headers or add data.
      */
-    public static func sendRequest<T>(_ method: Method = .GET, toUrl path: String, type: T.Type, action: @escaping (inout URLRequest) -> Void = { _ in }) -> T? where T: Decodable {
+    public static func sendRequest<T>(_ method: Http.Method = .GET, toUrl path: String, type: T.Type, action: @escaping (inout URLRequest) -> Void = { _ in }) -> (T?, Int?, Http.Error?) where T: Decodable {
         guard let url = URL(string: path) else {
-            print("failed to generate url from string: \(path)")
-            return nil
+            #if DEBUG
+            print("[sendRequest: debug] failed to generate url from string: \(path)")
+            #endif
+            return (nil, nil, .errParseURL)
         }
+        
         let channel = DispatchSemaphore(value: 0)
         var result: T? = nil
+        var code: Int? = nil
+        var err: Http.Error? = nil
         
         var request = URLRequest(url: url, timeoutInterval: 30)
-        action(&request)
         request.httpMethod = method.rawValue
+        action(&request)
         
-        URLSession.shared.dataTask(with: request) { data, responce, error in
+        URLSession.shared.dataTask(with: request) { data, response, error in
             defer { channel.signal() }
-            guard
-                let data = data,
-                error == nil ,
-                let responce = responce as? HTTPURLResponse,
-                responce.statusCode == 200 else {
-                print("Failed to download data")
+            guard error == nil else {
+                err = .errDownloadData(String(describing: error))
                 return
             }
-            print("complete download, data length: \(data.count)")
+            
+            guard let res = response as? HTTPURLResponse else {
+                err = .errDownloadData("network connection error")
+                return
+            }
+            
+            code = res.statusCode
+          
+            guard let data = data else {
+                err = .errDownloadData("connection timeout")
+                return
+            }
+            
+            #if DEBUG
+                print("[sendRequest: debug] complete download, data length: \(data.count)")
+                print("[sendRequest: debug] data: \n\(String(decoding: data, as: UTF8.self))")
+            #endif
+            
             do {
                 let decoded = try JSONDecoder().decode(T.self, from: data)
                 result = decoded
             } catch {
-                print("decode data error, \(error)")
+                #if DEBUG
+                print("[sendRequest: debug] decode data error, \(String(describing: error))")
+                #endif
+                err = .errDecodeData(String(describing: error))
             }
             
         }.resume()
+        
         channel.wait()
-        return result
+        return (result, code, err)
     }
     
     /**
@@ -66,75 +110,63 @@ extension Http {
      
      ```swift
      let url = "http://api/user"
-     let body = Http.DumpRequest(.GET, toUrl: url, type: User.self) { request in
-     // do something ...
+     let (body, statusCode, err) = Http.sendRequest(.GET, toUrl: url, type: User.self) { request in
+        // do something ...
      }
      ```
+     - parameters:
+        - method: The http method for this request. (GET, POST, PUT, PATCH, DELETE)
+        - toUrl: The url to send request.
+        - ignoreBody: It won't return the body if true. It helps increasing performance to ignore the body return.
+        - action: Use this to modify request, such as set cookies, add headers or add data.
      */
-    public static func dumpRequest(_ method: Method = .GET, toUrl path: String, action: @escaping (inout URLRequest) -> Void = { _ in }) -> String {
+    public static func sendRequest(_ method: Method = .GET, toUrl path: String, ignoreBody: Bool = false, action: @escaping (inout URLRequest) -> Void = { _ in }) -> (String, Int?, Http.Error?) {
         guard let url = URL(string: path) else {
-            print("failed to generate url from string: \(path)")
-            return ""
+            #if DEBUG
+            print("[sendRequest: debug] failed to generate url from string: \(path)")
+            #endif
+            return ("", nil, .errParseURL)
         }
+        
         let channel = DispatchSemaphore(value: 0)
         var result: String = ""
+        var code: Int? = nil
+        var err: Http.Error? = nil
         
         var request = URLRequest(url: url, timeoutInterval: 30)
-        action(&request)
         request.httpMethod = method.rawValue
+        action(&request)
         
-        URLSession.shared.dataTask(with: request) { data, responce, error in
+        URLSession.shared.dataTask(with: request) { data, response, error in
             defer { channel.signal() }
-            guard
-                let data = data,
-                error == nil ,
-                let responce = responce as? HTTPURLResponse,
-                responce.statusCode == 200 else {
-                print("Failed to download data")
+            guard error == nil else {
+                err = .errDownloadData(String(describing: error))
                 return
             }
-            print("complete download, data length: \(data.count)")
-            result = String(decoding: data, as: UTF8.self)
+            
+            guard let res = response as? HTTPURLResponse else {
+                err = .errDownloadData("network connection error")
+                return
+            }
+            
+            code = res.statusCode
+            
+            guard let data = data else {
+                err = .errDownloadData("connection timeout")
+                return
+            }
+            
+            #if DEBUG
+                print("[sendRequest: debug] complete download, data length: \(data.count)")
+            #endif
+            
+            if !ignoreBody {
+                result = String(decoding: data, as: UTF8.self)
+            }
             
         }.resume()
         channel.wait()
-        return result
-    }
-    
-    /**
-     Send a request and get status code
-     
-     ```swift
-     let url = "http://api/user"
-     let code = Http.RequestStatusCode(.GET, toUrl: url, type: User.self) { request in
-     // do something ...
-     }
-     ```
-     */
-    public static func requestStatusCode(_ method: Method = .GET, toUrl path: String, action: @escaping (inout URLRequest) -> Void = { _ in }) -> Int? {
-        guard let url = URL(string: path) else {
-            print("failed to generate url from string: \(path)")
-            return nil
-        }
-        let channel = DispatchSemaphore(value: 0)
-        var result: Int? = nil
         
-        var request = URLRequest(url: url, timeoutInterval: 30)
-        action(&request)
-        request.httpMethod = method.rawValue
-        
-        URLSession.shared.dataTask(with: request) { _, responce, error in
-            defer { channel.signal() }
-            guard
-                error == nil ,
-                let responce = responce as? HTTPURLResponse else {
-                print("Failed to download data")
-                return
-            }
-            result = responce.statusCode
-            
-        }.resume()
-        channel.wait()
-        return result
+        return (result, code, err)
     }
 }

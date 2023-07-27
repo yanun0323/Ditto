1d2b5a889061ffdc9e9fb1f3cd51aaf0ff48d76c
Yanun <yanunyang@Yanuns-MacBook-Air-15.local>
2023-07-28 05:11:13 +0800

    udpate Block init func

--- Sources/Ditto/UI/Block.swift
+++ Sources/Ditto/UI/Block.swift
@@ -12,6 +12,12 @@ public struct Block: View {
         self._color = .init(wrappedValue: color)
     }
     
+    public init(size: CGSize, color: Color = .clear) {
+        self._width = .init(wrappedValue: size.width)
+        self._height = .init(wrappedValue: size.height)
+        self._color = .init(wrappedValue: color)
+    }
+    
     public var body: some View {
         Rectangle()
             .foregroundColor(color)

baca05cd9942ce9fcac1d2753cba0d6bc60d19fe
yanun0323 <a1256212562@gmail.com>
2023-10-04 21:57:55 +0800

    adjust statusbar height

--- Sources/Ditto/Model/Room.swift
+++ Sources/Ditto/Model/Room.swift
@@ -6,7 +6,7 @@ import SwiftUI
 @available(iOS 16, macOS 13, watchOS 9, *)
 public struct Room {
     public static let device = System.screen
-    public static let statusbar = CGSize(width: device.width, height: 100)
+    public static let statusbar = CGSize(width: device.width, height: 80)
     public static let container = CGSize(width: device.width, height: device.height - statusbar.height - homebar.height)
     public static let homebar = CGSize(width: device.width, height: 35)
 }

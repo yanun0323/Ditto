9f2576e760c8991aebf0dbf8369958803d099253
yanun0323 <a1256212562@gmail.com>
2023-10-21 01:33:43 +0800

    udpate safe area property

--- Sources/Ditto/Model/SafeArea.swift
+++ Sources/Ditto/Model/SafeArea.swift
@@ -7,17 +7,10 @@ import AppKit
 
 #if os(iOS) || os(macOS) || os(watchOS)
 public struct Device {
-    public let screen: CGSize
-    public let homebarArea: CGSize
-    public let statusbarArea: CGSize
-    public let safeArea: CGSize
-    
-    init() {
-        self.screen = Device.screen
-        self.homebarArea = Device.homebarArea
-        self.statusbarArea = Device.statusbarArea
-        self.safeArea = Device.safeArea
-    }
+    public var screen: CGSize { Device.screen }
+    public var homebarArea: CGSize { Device.homebarArea }
+    public var statusbarArea: CGSize { Device.statusbarArea }
+    public var safeArea: CGSize { Device.safeArea }
 }
 
 extension Device {
@@ -39,12 +32,13 @@ public extension EdgeInsets {
 
 #if os(watchOS)
 extension Device {
-    public static let screen: CGSize = WKInterfaceDevice.current().screenBounds.screenSize
-    public static let safeAreaInsets: EdgeInsets = .init()}
+    public static var screen: CGSize { WKInterfaceDevice.current().screenBounds.screenSize }
+    public static var safeAreaInsets: EdgeInsets { .init() }
+}
 #elseif os(iOS)
 extension Device {
-    public static let screen: CGSize = UIScreen.main.bounds.size
-    public static let safeAreaInsets: EdgeInsets = UIApplication.shared.rootWindow?.safeAreaInsets.insets ?? .init()
+    public static var screen: CGSize { UIScreen.main.bounds.size }
+    public static var safeAreaInsets: EdgeInsets { UIApplication.shared.rootWindow?.safeAreaInsets.insets ?? .init() }
 }
 
 extension UIEdgeInsets {
@@ -54,40 +48,38 @@ extension UIEdgeInsets {
 }
 
 extension UIApplication {
-    public var keyWindow: UIWindow? {
-        connectedScenes
-            .compactMap {
-                $0 as? UIWindowScene
-            }
-            .flatMap {
-                $0.windows
-            }
-            .first {
-                $0.isKeyWindow
-            }
-    }
-    
-    
     public var rootWindow: UIWindow? {
         connectedScenes
-            .compactMap {
-                $0 as? UIWindowScene
-            }
-            .flatMap {
-                $0.windows
-            }
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
             .first
     }
 }
 #elseif os(macOS)
 extension Device {
-    public static let screen: CGSize = NSScreen.main?.visibleFrame.screenSize ?? .zero // You could implement this to force a CGFloat and get the full device screen size width regardless of the window size with .frame.size.width
-    public static let safeAreaInsets: EdgeInsets = NSScreen.main?.safeAreaInsets.insets ?? .init()
+    public static var screen: CGSize { NSScreen.main?.visibleFrame.screenSize ?? .zero } // You could implement this to force a CGFloat and get the full device screen size width regardless of the window size with .frame.size.width
+    public static var safeAreaInsets: EdgeInsets { NSScreen.main?.safeAreaInsets.insets ?? .init() }
 }
 
 extension NSEdgeInsets {
     public var insets: EdgeInsets {
         EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
     }
+}
+#endif
+
+#if DEBUG
+struct SafeAreaTestView: View {
+    var body: some View {
+        SwiftUI.Button("print parameter") {
+            print("\(Device.safeArea)")
+            print("\(Device.statusbarArea)")
+            print("\(Device.homebarArea)")
+        }
+    }
+}
+
+#Preview {
+    SafeAreaTestView()
 }
 #endif

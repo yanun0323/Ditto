8755a414bdfdd89418c49b5e9161540b92cff518
yanun0323 <a1256212562@gmail.com>
2023-10-22 03:57:01 +0800

    update tab picker

--- Sources/Ditto/UI/TabPicker.swift
+++ Sources/Ditto/UI/TabPicker.swift
@@ -1,29 +1,40 @@
 import SwiftUI
 
+public struct TabPickerItem: Identifiable {
+    public var id: String { self.image }
+    public var action: (() -> Void)? = nil
+    public var image: String
+}
+
 public struct TabPicker: View {
     @Binding var selection: Int
-    @State var size = CGFloat(22)
-    @State var color = Color.accentColor
-    @State var items: [String]
+    @State var size: CGFloat
+    @State var color: Color
+    @State var items: [TabPickerItem]
+    
+    init(selection: Binding<Int>, size: CGFloat = CGFloat(22), color: Color = Color.accentColor, items: [TabPickerItem]) {
+        self._selection = selection
+        self.size = size
+        self.color = color
+        self.items = items
+    }
     
     public var body: some View {
         HStack(spacing: 0) {
             Spacer()
-            ForEach(items.indices, id: \.self) { i in
+            ForEach(0 ..< items.count, id: \.self) { i in
                 Button(width: size, height: size) {
-                    
+                    selection = i
                 } content: {
-                    Image(systemName: items[i])
+                    Image(systemName: items[i].image)
                         .font(.system(size: size*0.8))
-                        .debug(.blue)
+                        .foregroundStyle(i == selection ? color : color.opacity(0.25))
                 }
-                .debug()
                 .tag(i)
 
                 Spacer()
             }
         }
-        .frame(height: size + 15)
     }
 }
 
@@ -33,18 +44,30 @@ struct TabPicker_Previews: PreviewProvider {
     
     static var previews: some View {
         VStack {
-            TabPicker(selection: $selected, items: [
-                "trash", "trash.fill"
+            TabPickerTest(items: [
+                TabPickerItem(image: "trash"),
+                TabPickerItem(image: "trash.fill")
             ])
             .debug()
             .frame(size: CGSize(width: 300, height: 50))
             
-            TabPicker(selection: $selected, items: [
-                "trash", "trash.fill"
+            TabPickerTest(size: 30, color: .primary, items: [
+                TabPickerItem(image: "trash"),
+                TabPickerItem(image: "trash.fill")
             ])
             .debug()
             .frame(size: CGSize(width: 300, height: 50))
         }
+    }
+}
+
+struct TabPickerTest: View {
+    @State private var selected: Int = 0
+    @State var size: CGFloat = 22
+    @State var color: Color = .accentColor
+    @State var items: [TabPickerItem]
+    var body: some View {
+        TabPicker(selection: $selected, size: size, color: color, items: items)
     }
 }
 #endif

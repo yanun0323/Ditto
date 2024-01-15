e2603ead78414e4673fa7607e14b09e81edf87d8
yanun0323 <a1256212562@gmail.com>
2024-01-15 22:31:22 +0800

    add ditto font and view extension

--- Sources/Ditto/Ditto.swift
+++ Sources/Ditto/Ditto.swift
@@ -1,11 +1,10 @@
 import Foundation
 import SwiftUI
 
-public struct Ditto {
-    @MainActor
+public class Ditto {
     public static func registerFonts() {
-        FontName.allCases.forEach {
-            registerFont(bundle: .module, fontName: $0.rawValue, fontExtension: "ttf")
+        DittoFont.allCases.forEach {
+            registerFont(bundle: .module, fontName: $0.filename, fontExtension: "ttf")
         }
     }
     
@@ -23,38 +22,20 @@ public struct Ditto {
         }
         
         if succeed {
-            print("font \(fontName) registered")
+            print("font \(font.fullName.string) registered")
         } else {
             fatalError("font \(fontName) register failed")
         }
     }
 }
 
-public enum FontName: String, CaseIterable {
-    case ChenYuluoyan_Thin_Monospaced = "ChenYuluoyan-Thin-Monospaced"
-    case Cubic11R = "Cubic_11_R"
-    case Huninn = "Huninn"
-    case IansuiRegular = "Iansui-Regular"
-    case NotoSansTCBlack = "NotoSansTC-Black"
-    case NotoSansTCBold = "NotoSansTC-Bold"
-    case NotoSansTCExtraBold = "NotoSansTC-ExtraBold"
-    case NotoSansTCExtraLight = "NotoSansTC-ExtraLight"
-    case NotoSansTCLight = "NotoSansTC-Light"
-    case NotoSansTCMedium = "NotoSansTC-Medium"
-    case NotoSansTCRegular = "NotoSansTC-Regular"
-    case NotoSansTCSemiBold = "NotoSansTC-SemiBold"
-    case NotoSansTCThin = "NotoSansTC-Thin"
-    case RobotoBlack = "Roboto-Black"
-    case RobotoBlackItalic = "Roboto-BlackItalic"
-    case RobotoBold = "Roboto-Bold"
-    case RobotoBoldItalic = "Roboto-BoldItalic"
-    case RobotoItalic = "Roboto-Italic"
-    case RobotoLight = "Roboto-Light"
-    case RobotoLightItalic = "Roboto-LightItalic"
-    case RobotoMedium = "Roboto-Medium"
-    case RobotoMediumItalic = "Roboto-MediumItalic"
-    case RobotoRegular = "Roboto-Regular"
-    case RobotoThin = "Roboto-Thin"
-    case RobotoThinItalic = "Roboto-ThinItalic"
+fileprivate extension CFString? {
+    var string: String {
+        if let s = self {
+            return "\(s)"
+        }
+        
+        return "-"
+    }
 }
 
--- Sources/Ditto/Extension/Font.swift
+++ Sources/Ditto/Extension/Font.swift
@@ -1,11 +1,170 @@
 import SwiftUI
 
+public enum DittoFont: CaseIterable {
+    case Cubic11R
+    case Huninn
+    case Iansui
+    case NotoSansTCBlack
+    case NotoSansTCBold
+    case NotoSansTCExtraBold
+    case NotoSansTCExtraLight
+    case NotoSansTCLight
+    case NotoSansTCMedium
+    case NotoSansTC
+    case NotoSansTCSemiBold
+    case NotoSansTCThin
+    case RobotoBlack
+    case RobotoBlackItalic
+    case RobotoBold
+    case RobotoBoldItalic
+    case RobotoItalic
+    case RobotoLight
+    case RobotoLightItalic
+    case RobotoMedium
+    case RobotoMediumItalic
+    case Roboto
+    case RobotoThin
+    case RobotoThinItalic
+    
+    var filename: String {
+        switch self {
+        case .Cubic11R:
+            return "Cubic_11_R"
+        case .Huninn:
+            return "Huninn"
+        case .Iansui:
+            return "Iansui-Regular"
+        case .NotoSansTCBlack:
+            return "NotoSansTC-Black"
+        case .NotoSansTCBold:
+            return "NotoSansTC-Bold"
+        case .NotoSansTCExtraBold:
+            return "NotoSansTC-ExtraBold"
+        case .NotoSansTCExtraLight:
+            return "NotoSansTC-ExtraLight"
+        case .NotoSansTCLight:
+            return "NotoSansTC-Light"
+        case .NotoSansTCMedium:
+            return "NotoSansTC-Medium"
+        case .NotoSansTC:
+            return "NotoSansTC-Regular"
+        case .NotoSansTCSemiBold:
+            return "NotoSansTC-SemiBold"
+        case .NotoSansTCThin:
+            return "NotoSansTC-Thin"
+        case .RobotoBlack:
+            return "Roboto-Black"
+        case .RobotoBlackItalic:
+            return "Roboto-BlackItalic"
+        case .RobotoBold:
+            return "Roboto-Bold"
+        case .RobotoBoldItalic:
+            return "Roboto-BoldItalic"
+        case .RobotoItalic:
+            return "Roboto-Italic"
+        case .RobotoLight:
+            return "Roboto-Light"
+        case .RobotoLightItalic:
+            return "Roboto-LightItalic"
+        case .RobotoMedium:
+            return "Roboto-Medium"
+        case .RobotoMediumItalic:
+            return "Roboto-MediumItalic"
+        case .Roboto:
+            return "Roboto-Regular"
+        case .RobotoThin:
+            return "Roboto-Thin"
+        case .RobotoThinItalic:
+            return "Roboto-ThinItalic"
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .Cubic11R:
+            return "Cubic 11"
+        case .Huninn:
+            return "jf-openhuninn-2.0"
+        case .Iansui:
+            return "Iansui Regular"
+        case .NotoSansTCBlack:
+            return "Noto Sans TC Black"
+        case .NotoSansTCBold:
+            return "Noto Sans TC Bold"
+        case .NotoSansTCExtraBold:
+            return "Noto Sans TC ExtraBold"
+        case .NotoSansTCExtraLight:
+            return "Noto Sans TC ExtraLight"
+        case .NotoSansTCLight:
+            return "Noto Sans TC Light"
+        case .NotoSansTCMedium:
+            return "Noto Sans TC Medium"
+        case .NotoSansTC:
+            return "Noto Sans TC Regular"
+        case .NotoSansTCSemiBold:
+            return "Noto Sans TC SemiBold"
+        case .NotoSansTCThin:
+            return "Noto Sans TC Thin"
+        case .RobotoBlack:
+            return "Roboto Black"
+        case .RobotoBlackItalic:
+            return "Roboto Black Italic"
+        case .RobotoBold:
+            return "Roboto Bold"
+        case .RobotoBoldItalic:
+            return "Roboto Bold Italic"
+        case .RobotoItalic:
+            return "Roboto Italic"
+        case .RobotoLight:
+            return "Roboto Light"
+        case .RobotoLightItalic:
+            return "Roboto Light Italic"
+        case .RobotoMedium:
+            return "Roboto Medium"
+        case .RobotoMediumItalic:
+            return "Roboto Medium Italic"
+        case .Roboto:
+            return "Roboto"
+        case .RobotoThin:
+            return "Roboto Thin"
+        case .RobotoThinItalic:
+            return "Roboto Thin Italic"
+        }
+    }
+}
 
+extension View {
+    @ViewBuilder
+    public func font(name font: DittoFont, size: CGFloat? = nil) -> some View {
+        if let size = size {
+            self.font(.custom(font.name, size: size))
+        } else {
+            #if os(macOS)
+            self.font(.custom(font.name, size: 13))
+            #else
+            self.font(.custom(font.name, size: 17))
+            #endif
+        }
+        
+    }
+}
+
+#if DEBUG
 #Preview {
     Ditto.registerFonts()
     return VStack {
         Text("Font Test 測試")
-            .font(.custom(FontName.Cubic11R.rawValue, size: 30, relativeTo: .body))
-            .paddings(30)
+            .font(name: .Roboto)
+        Text("Font Test 測試")
+            .font(.custom(DittoFont.Cubic11R.name, size: 17))
+        Text("Font Test 測試")
+            .font(.custom(DittoFont.Cubic11R.name, fixedSize: 17))
+        Text("Font Test 測試")
+            .font(.body)
+        Text("Font Test 測試")
+            .font(.system(size: 17))
     }
+    .paddings(30)
+//    .dynamicTypeSize(.xLarge)
 }
+#endif

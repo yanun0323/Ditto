74a1eb29d8a31f109e989fbde504b92ddadb6b9e
yanun0323 <a1256212562@gmail.com>
2024-01-23 20:28:27 +0800

    refactor date extension

--- Sources/Ditto/Extension/Date.swift
+++ Sources/Ditto/Extension/Date.swift
@@ -1,194 +1,189 @@
 import Foundation
 import SwiftUI
 
-// MARK: Date Static function
+// MARK: - SpanComponent
+public enum SpanComponent {
+    case year, month, day, hour, minute, second
+
+    public var calendarComponent: Calendar.Component {
+        switch self {
+        case .year: return .year
+        case .month: return .month
+        case .day: return .day
+        case .hour: return .hour
+        case .minute: return .minute
+        case .second: return .second
+        }
+    }
+}
+
+// MARK: - Date
 extension Date {
-    public init?(from date: String, _ layout: DateFormatLayout, _ locale: Locale = Locale.current, _ timezone: TimeZone? = nil) {
+    public init(_ year: Int, _ month: Int, _ day: Int, _ hour: Int, _ minute: Int, _ second: Int, locale: Locale = .autoupdatingCurrent) {
+        var comp = DateComponents()
+        comp.year = year
+        comp.month = month
+        comp.day = day
+        comp.hour = hour
+        comp.minute = minute
+        comp.second = second
+        comp.timeZone = locale.timeZone
+        self = comp.date!
+    }
+
+    public init?(from date: String, _ layout: DateFormatLayout, _ locale: Locale = .autoupdatingCurrent) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = layout
         dateFormatter.locale = locale
-        dateFormatter.timeZone = timezone
+        dateFormatter.timeZone = locale.timeZone
         guard let result = dateFormatter.date(from: date) else { return nil }
         self = result
     }
-    
-    public init(_ unixDay: Int) {
-        self = Date(timeIntervalSince1970: TimeInterval(unixDay * 86_400))
-    }
-    
-    public static let zero = Date(timeIntervalSince1970: 0)
 }
 
-// MARK: Date Property
+// MARK: Date Static
+extension Date { public static let zero = Date(timeIntervalSince1970: 0) }
+
 extension Date {
-    /** Return the second for 1970-01-01 00:00:00 UTC*/
-    public var unix: Int {
-        self.timeIntervalSince1970.seconds
-    }
-    
-    /** Return the day for 1970-01-01 UTC*/
-    public var unixDay: Int {
-        self.timeIntervalSince1970.days
-    }
-    
+    public var year: Int { Calendar.current.component(.year, from: self) }
+    public var month: Int { Calendar.current.component(.month, from: self) }
+    public var day: Int { Calendar.current.component(.day, from: self) }
+    public var hour: Int { Calendar.current.component(.hour, from: self) }
+    public var minute: Int { Calendar.current.component(.minute, from: self) }
+    public var second: Int { Calendar.current.component(.second, from: self) }
+    public var monthFirst: Date { Date(self.year, self.month, 1, 0, 0, 0) }
+    public var monthLast: Date { monthFirst.addMonth(1).addDay(-1) }
+
+    /** Return the second for 1970-01-01 00:00:00 UTC */
+    public var unix: Int { self.timeIntervalSince1970.seconds }
+    /** Return the day for 1970-01-01 UTC */
+    public var unixDay: Int { self.timeIntervalSince1970.days }
+
     /** Return the day of the week
-     
      0 = Sunday, 1 = Monday, ... 6 = Saturday
      */
-    public var dayOfWeekDay: Int {
-        (self.unixDay + 5) % 7
-    }
-    
-    public var isToday: Bool {
-        self.unixDay == Date.now.unixDay
-    }
-    
-    public var daysOfMonth: Int {
-        let first = self.firstDayOfMonth
-        return first.distance(to: first.addMonth(1)).days
-    }
-    
-    public var weeksOfMonth: Int {
-        guard let first = Self.init(from: "\(self.string("yyyyMM"))01", .Numeric) else { return -1 }
+    public var weekDay: Int { (self.unixDay + 5) % 7 }
+
+    public var isToday: Bool { self.year == Date.now.year && self.month == Date.now.month && self.day == Date.now.day }
+
+    /** Return the week's count of the month  */
+    public var monthWeeks: Int {
+        let first = self.monthFirst
         let firstWeekDay = (first.timeIntervalSince1970.days + 5) % 7
-        let days = first.distance(to: first.addMonth(1)).days
+        let days = first.between(.day, first.addMonth(1))
         return (days + firstWeekDay + 6) / 7
     }
-    
-    public var firstDayOfMonth: Date {
-        Self.init(from: "\(self.string("yyyyMM", .us))01", .Numeric)!
-    }
-    
-    public var lastDayOfMonth: Date {
-        self.firstDayOfMonth.addMonth(1).addDay(-1)
+
+    /** Return the day's count of the month  */
+    public var monthDays: Int {
+        let first = Date(self.year, self.month, 1, 0, 0, 0)
+        let next = Calendar.current.date(byAdding: .month, value: 1, to: first) ?? first
+        let set = Set<Calendar.Component>([.day])
+        return Calendar.current.dateComponents(set, from: first, to: next).day ?? 1
     }
 }
 
 // MARK: Date Function
 extension Date {
-    public func string(_ layout: DateFormatLayout = .Default, _ locale: Locale = .current, _ timezone: TimeZone? = nil) -> String
-    {
+    public func string(_ layout: DateFormatLayout = .Default, _ locale: Locale = .autoupdatingCurrent) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = layout
         dateFormatter.locale = locale
-        dateFormatter.timeZone = timezone
+        dateFormatter.timeZone = locale.timeZone
         return dateFormatter.string(from: self)
     }
-    
-    public func addDay(_ day: Int) -> Date {
-        return self.add(.day, day)
-    }
-    
-    public func addMonth(_ month: Int) -> Date {
-        return self.add(.month, month)
-    }
-    
-    public func addYear(_ year: Int) -> Date {
-        return self.add(.year, year)
-    }
-    
-    public func addWeek(_ week: Int) -> Date {
-        return self.add(.day, 7 * week)
-    }
-    
-    public func add(_ unit: Calendar.Component, _ value: Int) -> Date {
-        return Calendar.current.date(byAdding: unit, value: value, to: self) ?? self
-    }
-    
+
+    public func addDay(_ day: Int) -> Date { return self.add(.day, day) }
+    public func addMonth(_ month: Int) -> Date { return self.add(.month, month) }
+    public func addYear(_ year: Int) -> Date { return self.add(.year, year) }
+    public func addWeek(_ week: Int) -> Date { return self.add(.day, 7 * week) }
+    public func add(_ unit: Calendar.Component, _ value: Int) -> Date { return Calendar.current.date(byAdding: unit, value: value, to: self) ?? self }
     public func add(_ span: DateSpan) -> Date? {
         switch span {
-            case .none:
-                return nil
-            case .day:
-                return self.addDay(1)
-            case .week:
-                return self.addWeek(1)
-            case .month:
-                return self.addMonth(1)
-            case .year:
-                return self.addYear(1)
+        case .none: return nil
+        case .day: return self.addDay(1)
+        case .week: return self.addWeek(1)
+        case .month: return self.addMonth(1)
+        case .year: return self.addYear(1)
         }
     }
-    
-    public func daysBetween(_ date: Date) -> Int {
-        return self.distance(to: date).days
+
+    public func between(_ unit: SpanComponent, _ to: Date) -> Int {
+        let sets = Set<Calendar.Component>([unit.calendarComponent])
+        let comp = Calendar.current.dateComponents(sets, from: self, to: to)
+        switch unit {
+        case .year: return comp.year ?? 0
+        case .month: return comp.month ?? 0
+        case .day: return comp.day ?? 0
+        case .hour: return comp.hour ?? 0
+        case .minute: return comp.minute ?? 0
+        case .second: return comp.second ?? 0
+        }
     }
 }
 
 // MARK: - DateFormatLayout
 public typealias DateFormatLayout = String
 
-public extension DateFormatLayout {
-    init(_ layout: String) {
-        self = layout
-    }
+extension DateFormatLayout {
+    public init(_ layout: String) { self = layout }
     /**
      2006-01-02 15:04:05 +0800
      */
-    static let Default: Self = "yyyy-MM-dd HH:mm:ss Z"
+    public static let Default: Self = "yyyy-MM-dd HH:mm:ss Z"
     /**
      Mon Jan 02 15:04:05 2006
      */
-    static let ANSIC: Self = "EE MMM dd HH:mm:ss yyyy"
+    public static let ANSIC: Self = "EE MMM dd HH:mm:ss yyyy"
     /**
      Mon Jan 02 15:04:05 +0800 2006
      */
-    static let UnixDate: Self = "EE MMM dd HH:mm:ss Z yyyy"
+    public static let UnixDate: Self = "EE MMM dd HH:mm:ss Z yyyy"
     /**
      02 Jan 06 15:04 +0800
      */
-    static let RFC822: Self = "dd MMM yy HH:mm Z"
+    public static let RFC822: Self = "dd MMM yy HH:mm Z"
     /**
      Mon, 02 Jan 2006 15:04:05 +0800
      */
-    static let RFC1123: Self = "EE, dd MMM yyyy HH:mm:ss Z"
+    public static let RFC1123: Self = "EE, dd MMM yyyy HH:mm:ss Z"
     /**
      Jan 02 15:04:05
      */
-    static let Stamp: Self = "MMM dd HH:mm:ss"
+    public static let Stamp: Self = "MMM dd HH:mm:ss"
     /**
      2006-01-02
      */
-    static let Date: Self = "yyyy-MM-dd"
+    public static let Date: Self = "yyyy-MM-dd"
     /**
      20060102
      */
-    static let Numeric: Self = "yyyyMMdd"
+    public static let Numeric: Self = "yyyyMMdd"
 }
 
+// MARK: - DateSpan
 public enum DateSpan: Int, Identifiable, Hashable, CaseIterable, Codable {
     public var id: Int { self.rawValue }
-    case none=0, day=1, week=2, month=3, year=4
-    
+    case none = 0
+    case day = 1
+    case week = 2
+    case month = 3
+    case year = 4
+
     init(_ int: Int) {
         switch int {
-            case Self.day.rawValue:
-                self = .day
-            case Self.week.rawValue:
-                self = .week
-            case Self.month.rawValue:
-                self = .month
-            case Self.year.rawValue:
-                self = .year
-            default:
-                self = .none
+        case Self.day.rawValue: self = .day
+        case Self.week.rawValue: self = .week
+        case Self.month.rawValue: self = .month
+        case Self.year.rawValue: self = .year
+        default: self = .none
         }
     }
 }
 
 // MARK: - TimeInterval
 extension TimeInterval {
-    
-    public var seconds: Int {
-        return Int(self)
-    }
-    
-    public var days: Int {
-        return self.seconds/86_400
-    }
-    
-    public var milliseconds: Int {
-        return Int(self * 1_000)
-    }
+    public var seconds: Int { return Int(self) }
+    public var days: Int { return self.seconds / 86_400 }
+    public var milliseconds: Int { return Int(self * 1_000) }
 }
-

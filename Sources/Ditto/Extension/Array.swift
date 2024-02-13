f4e149aae0324bfae6dc4c15d7fb23a1ae71c744
yanun0323 <a1256212562@gmail.com>
2024-02-13 16:08:27 +0800

    fix: rename forEach to forEachIndexed

--- Sources/Ditto/Extension/Array.swift
+++ Sources/Ditto/Extension/Array.swift
@@ -3,7 +3,7 @@ import SwiftUI
 extension Array {
     public var indexes: Range<Int> { 0 ..< self.count}
     
-    public func forEach(_ body: (Int, Element) throws -> Void) rethrows {
+    public func forEachIndexed(_ body: (Int, Element) throws -> Void) rethrows {
         for i in indexes {
             try body(i, self[i])
         }

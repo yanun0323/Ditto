eed216ed83b802a4a094557bdb9594f9f3c04bc2
yanun0323 <a1256212562@gmail.com>
2024-02-13 16:25:40 +0800

    feat: array appended function

--- Sources/Ditto/Extension/Array.swift
+++ Sources/Ditto/Extension/Array.swift
@@ -8,4 +8,16 @@ extension Array {
             try body(i, self[i])
         }
     }
+    
+    public func appended(_ newElement: Element) -> Self {
+        var result = self
+        result.append(newElement)
+        return result
+    }
+    
+    public func appended<S>(contentsOf newElements: S) -> Self where Element == S.Element, S : Sequence {
+        var result = self
+        result.append(contentsOf: newElements)
+        return result
+    }
 }

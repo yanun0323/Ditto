b86d28a8480c8df93bca919e2fc62563ff7d9b2e
yanun0323 <a1256212562@gmail.com>
2024-02-21 01:23:52 +0800

    release version 0.8.18

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Ditto",
-            targets: ["Ditto"])
+            targets: ["Ditto"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,10 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Ditto",
-            dependencies: [
-            ],
             resources: [
-                .process("Font"),
+//                .process("Font"),
+                .copy("Font"),
             ]
         ),
         .testTarget(
